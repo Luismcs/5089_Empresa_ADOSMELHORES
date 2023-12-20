2a05d4bf7e94ca4bff7f9d2257b6c5b6b04a0fda
Luismcs <131809527+Luismcs@users.noreply.github.com>
2023-12-20 12:21:42 +0000

    comit teste

--- Empresa.cs
+++ Empresa.cs
@@ -8,104 +8,94 @@ using System.IO;
 
 namespace ConsoleApp1
 {
-    internal class FileHandler
-    {
-        public static void WriteToFile(string frase, string nome)
-        {
-            StreamWriter escrita = new StreamWriter(@"funcionarios.txt"); //cria variavel de escrita para um ficheiro
-            escrita.WriteLine(frase);
-            escrita.Close();
-        }
 
-        public static void ReadFromFile(string nomeficheiro)
-        {
-            StreamReader leitura = new StreamReader(nomeficheiro); //cria variavel de leitura para um ficheiro
-            while (!leitura.EndOfStream) //Lê linhas enquanto não chegar ao fim do ficheiro de leitura
-            {
-                string linha = leitura.ReadLine(); //Lê a linha do ficheiro de leitura
-                Console.WriteLine(linha);
-            }
-
-            leitura.Close(); //fecha a variavel de leitura
-        }
-
-    }
     internal class Empresa
     {
-        private List<Funcionario> funcionarios; //cria uma lista de funcionários chamada funcionários
 
-        public List<Funcionario> Funcionarios //get e set da lista
-        {
-            get { return funcionarios; }
-            set { funcionarios = value; }
-        }
+
+
+
+        List<Funcionario> funcionarios = new List<Funcionario>();
+
 
         // Constructor
         public Empresa()
         {
 
-            funcionarios = new List<Funcionario>();
-
-           
             
+
+
         }
 
         //Método que adiciona funcionario á lista
-        public void AdicionarFuncionario(Funcionario funcionario)
-        {
-            funcionarios.Add(funcionario);
-        }
+    
 
 
-       //public void Selecao_horario()
-       // {   
-
-       //     Console.WriteLine("Qual e o funcionario que pretende selecionar?");
-       //     Console.WriteLine("Escolha a opção referente ao  horário de trabalho: ");
-       //     Console.WriteLine("1- Laboral - 8h-12h");
-       //     Console.WriteLine("2- Ambos - 8h-12h e 14h-18h");
-       //     Console.WriteLine("3- Pos-laboral 20h-24h e 2h-6h");
-       //     string horario = Console.ReadLine();
-       //     if (int.Parse(horario) == 1)
-       //     {
-       //         Console.WriteLine("Horário Laboral");
-                
-       //     }
-       //     else if (int.Parse(horario) == 2)
-       //     {
-       //         Console.WriteLine("Horário Ambos");
-       //     }
-       //     else if (int.Parse(horario) == 3)
-       //     {
-       //         Console.WriteLine("Horário Pos-laboral");
-       //     }
-       //     else
-       //     {
-       //         Console.WriteLine("Horário inválido");
-       //     }
-
-            
-            
-       // }
        public void LerFicheiro()
         {
+           string id;
+            string nome;
+            string morada;
+            string telefone;
+            string dataFim;
+            string dataRegisto;
+            string isencao;
+            string bonus;
+            string carro;
+            string chefe;
+            string area;
+            string disponibilidade;
+            string valorHora;
+
+
+            // Verificar se o arquivo existe antes de tentar lê-lo
             if (File.Exists("funcionarios.txt"))
             {
-                // Ler todas as linhas do arquivo "funcionarios.txt" e armazená-las em uma lista
-                List<string> linhasArquivo = new List<string>(File.ReadAllLines("funcionarios.txt"));
+                // Usar StreamReader para ler o arquivo linha por linha
+                using (StreamReader sr = new StreamReader("funcionarios.txt"))
+                {
+                    string linha;
+                    while (!sr.EndOfStream)
+                    {
+                        string[] vec_fun = sr.ReadLine().Split(';');
+                        id = vec_fun[0];
+                        nome = vec_fun[1];
+                        morada = vec_fun[2];
+                        telefone = vec_fun[3];
+                        dataFim = vec_fun[4];
+                        dataRegisto = vec_fun[5];
+                        isencao = vec_fun[6];
+                        bonus = vec_fun[7];
+                        carro = vec_fun[8];
+                        chefe = vec_fun[9];
+                        area = vec_fun[10];
+                        disponibilidade = vec_fun[11];
+                        valorHora = vec_fun[12];
+                        Funcionario funcionario = new Funcionario(id, nome, morada, telefone, dataFim, dataRegisto, isencao, bonus, carro, chefe, area, disponibilidade, valorHora);
+                        funcionarios.Add(funcionario);
+
+                        
+                        
+
+                    }
+
+                }
 
                 // Mostrar o conteúdo na console linha por linha
-                foreach (string linha in linhasArquivo)
+               Console.WriteLine("Lista de Funcionarios:");
+                foreach (Funcionario line in funcionarios)
                 {
-                    Console.WriteLine(linha);
+                    Console.WriteLine(line.ExibirInformacoes());
                 }
+
+                
             }
             else
             {
                 Console.WriteLine("O arquivo 'funcionarios.txt' não foi encontrado.");
             }
+
         }
     }
-
-    }
 }
+ 
--- Program.cs
+++ Program.cs
@@ -10,27 +10,11 @@ namespace ConsoleApp1
      
         static void Main(string[] args)
         {
-            {
-                //// Declarar uma lista de Funcionário
-                //List<Funcionario> funcionarios = new List<Funcionario>();
+            
 
-                //// Adicionar um novo funcionário à lista
-                //DateTime dataHoraAtual = DateTime.Now;
-                //Funcionario novoFuncionario = new Funcionario(1, "João", "RUA A", "91991919", dataHoraAtual, dataHoraAtual, "chefe1", "Area1", "Laboral", 20.5);
-                //Funcionario novoFuncionario2 = new Funcionario(1, "João", "RUA A", "91991919", dataHoraAtual, dataHoraAtual, "chefe1", "Area1", "Laboral", 20.5);
-                //Funcionario novoFuncionario3 = new Funcionario(1, "João", "RUA A", "91991919", dataHoraAtual, dataHoraAtual, "chefe1", "Area1", "Laboral", 20.5);
-
-                //funcionarios.Add(novoFuncionario);
-                //funcionarios.Add(novoFuncionario2);
-                //funcionarios.Add(novoFuncionario3);
-
-                //// Converter a lista de Funcionário em uma representação de texto
-                //List<string> linhas = funcionarios.Select(f => f.ConverterParaTexto()).ToList();
-
-                // Escrever a representação de texto em um arquivo
-                // Verificar se o arquivo existe antes de tentar lê-lo
                 Empresa ADOSMELHORES = new Empresa();
                 ADOSMELHORES.LerFicheiro();
+
         }
 
             
@@ -39,4 +23,4 @@ namespace ConsoleApp1
 
         }
     }
-}
+
