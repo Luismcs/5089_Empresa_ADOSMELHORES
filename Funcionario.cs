9a52e2f2ab09d2af551a36474c976cf8785b3755
Luismcs <131809527+Luismcs@users.noreply.github.com>
2023-12-20 13:26:02 +0000

    quartocommit

--- Funcionario.cs
+++ Funcionario.cs
@@ -11,85 +11,26 @@ namespace ConsoleApp1
     {
 
         //atributos da classe
+        public string Id { get; set; }
+        public string Nome { get; set; }
 
-        private string id;
-        private string nome;
-        private string morada;
-        private string telefone;
-        private string dataFim;
-        private string dataRegisto;
-        private string isencao;
-        private string bonus;
-        private string carro;
-        private string chefe;
-        private string area;
-        private string disponibilidade;
-        private string valorHora;
+        public string Morada { get; set; }
+        public string Telefone { get; set; }
+        public string _DataFim { get; set; }
+        public string _DataRegisto { get; set; }
+        public string _Isencao { get; set; }
+        public string _Bonus { get; set; }
+        public string _Carro { get; set; }
+        public string _Chefe { get; set; }
+        public string _Area { get; set; }
+        public string _Disponibilidade { get; set; }
+        public string _ValorHora { get; set; }
+
+        
         //public int Id { get; set; }
 
         //gets e sets
-        public string Id { 
-                        get { return id; } 
-                        set {  id = value; } 
-                      }
-        public string Nome { 
-                        get {  return nome; } 
-                        set {  nome = value; } 
-                      }
-        public string Morada {
-            get { return morada; }
-            set { morada = value; }
-        }
-        public string Telefone
-        {
-            get { return telefone; }
-            set { telefone = value; }
-        }
-        public string Data_fim
-        {
-            get { return dataFim; }
-            set { dataFim = value; }
-        }
-        public string Data_Registo
-        {
-            get { return dataRegisto; }
-            set { dataRegisto = value; }
-        }
-        public string Isencao
-        {
-            get { return isencao; }
-            set { isencao = value; }
-        }
-        public string Bonus
-        {
-            get { return bonus; }
-            set { bonus = value; }
-        }
-        public string Carro
-        {
-            get { return carro; }
-            set { carro = value; }
-        }
-        public string Chefe
-        {
-            get { return chefe; }
-            set { chefe = value; }
-        }
-        public string Area
-        {
-            get { return area; }
-            set { area = value; }
-        }
-        public string Disponibilidade
-        {
-            get { return disponibilidade; }
-            set { disponibilidade = value; }
-        }
-        public string Valor_Hora
-        {
-            get { return valorHora; }
-            set { valorHora = value; }
-        }
+        
 
         //Construtor
         public Funcionario(string id, string nome, string morada, string telefone,string DataFim, string DataRegisto,string isencao,string bonus,string carro,string Chefe, string Area, string Disponibilidade, string ValorHora)
@@ -98,15 +39,15 @@ namespace ConsoleApp1
             Nome = nome;
             Morada = morada;
             Telefone = telefone;
-            dataFim = DataFim;
-            dataRegisto = DataRegisto;
-            chefe = Chefe;
-            area=Area;
-            disponibilidade = Disponibilidade;
-            valorHora = ValorHora;
-            Isencao = isencao;
-            Bonus = bonus;   
-            Carro = carro;  
+            _DataFim = DataFim;
+            _DataRegisto = DataRegisto;
+            _Chefe = Chefe;
+            _Area=Area;
+            _Disponibilidade = Disponibilidade;
+            _ValorHora = ValorHora;
+            _Isencao = isencao;
+            _Bonus = bonus;   
+            _Carro = carro;  
         }
 
         // Método para exibir informações do funcionário
@@ -114,11 +55,11 @@ namespace ConsoleApp1
         {
             return
                               $"ID: {Id}\nNome: {Nome}\nMorada: {Morada}\n" +
-                              $"Contacto: {Telefone}\nFim de Contrato: {dataFim}\n" +
-                              $"Registo Criminal: {dataRegisto}\nIsenção de Horário: {Isencao}\n" +
-                              $"Bónus Mensal: {Bonus}\nCarro da Empresa: {Carro}\n" +
-                              $"Reporta a: {Chefe}\nÁrea: {Area}\nDisponibilidade: {Disponibilidade}\n" +
-                              $"Valor Hora: {valorHora}";
+                              $"Contacto: {Telefone}\nFim de Contrato: {_DataFim}\n" +
+                              $"Registo Criminal: {_DataRegisto}\nIsenção de Horário: {_Isencao}\n" +
+                              $"Bónus Mensal: {_Bonus}\nCarro da Empresa: {_Carro}\n" +
+                              $"Reporta a: {_Chefe}\nÁrea: {_Area}\nDisponibilidade: {_Disponibilidade}\n" +
+                              $"Valor Hora: {_ValorHora}";
                               
         }
         
